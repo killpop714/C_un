94f0d3f2beb6d9fa12430799383ddf1d74e8ffe3
killpop714 <112853297+killpop714@users.noreply.github.com>
2024-11-18 12:54:27 +0900

    대충

--- Project2/Project2/소스.cpp
+++ Project2/Project2/소스.cpp
@@ -2,10 +2,19 @@
 
 int main()
 {
-	int Sumdata = 0;
-	for (int i = 0; i < 5; i++)
+	int Sumdata = 1;
+	int n=0;
+
+	while (n <= 0)
 	{
-		Sumdata += i;
+		scanf_s("값을 입력하세요:%d", &n);
+		//if (n <= 0)
+			//printf("값을 잘못 입력하였습니다.");
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		Sumdata *= n;
 	}
 	return 0;
 }
