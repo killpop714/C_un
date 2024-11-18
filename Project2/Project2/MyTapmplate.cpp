550decb6297621725c23ac097ed47424acc63169
killpop714 <112853297+killpop714@users.noreply.github.com>
2024-11-18 13:58:35 +0900

    개인 개발

--- Project2/Project2/MyTapmplate.cpp
+++ Project2/Project2/MyTapmplate.cpp
@@ -4,11 +4,16 @@
 
 using namespace std;
 
-void main()
+int main()
 {
-	vector<int> vec = { 1,2,3,4,5 };
-	reverse(vec.begin, vec.end);
+	vector<int> vec = { 3,4,5,6,7 };
+
+	//정렬 함수
+	reverse(vec.begin(), vec.end());
+
+	//for  축약문으로 이러한 형식은 vec의 데이터가 num으로 넘어갔다.
 	for (int num : vec) {
 		cout << num << "";
 	}
+	return 0;
 }
