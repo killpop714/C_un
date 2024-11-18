04f6c6d7e7090d5db50e26b4c2d67423709db33a
killpop714 <112853297+killpop714@users.noreply.github.com>
2024-11-18 14:40:35 +0900

    끝

--- Project2/Project2/MyTapmplate.cpp
+++ Project2/Project2/MyTapmplate.cpp
@@ -39,3 +39,32 @@
 //	cout << a << endl;
 //	return 0;
 //}
+#include <iostream>
+
+
+using namespace std;
+
+template<typename T1, typename T2>
+struct Tree_node
+{
+	T1 key;
+	T2 value;
+	char color;
+
+	Tree_node<T1, T2>* left;
+	Tree_node<T1, T2>* right;
+	Tree_node<T1, T2>* parent;
+
+	Tree_node() { color = 'R';left=nullptr, right = nullptr
+	, parent = nullptr
+	}
+	Tree_node(T1 k, T2 v, char c, Tree_node* l, Tree_node* r, Tree_node* p) { key = k; value = v; color = c; left = l; right = r; parent = p; }
+	~Tree_node() {}
+};
+
+int main()
+{
+
+	
+	return 0;
+}
