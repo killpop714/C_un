17c6e46e8405f018a8ac481b6fc0c9c2a91bff35
killpop714 <112853297+killpop714@users.noreply.github.com>
2024-11-18 12:59:56 +0900

    11주차 1교시

--- Project2/Project2/소스.cpp
+++ Project2/Project2/소스.cpp
@@ -2,19 +2,20 @@
 
 int main()
 {
-	int Sumdata = 1;
+	double Sumdata = 1;
 	int n=0;
 
 	while (n <= 0)
 	{
-		scanf_s("값을 입력하세요:%d", &n);
-		//if (n <= 0)
-			//printf("값을 잘못 입력하였습니다.");
+		printf("값을 입력하세요:");
+		scanf_s("%d", &n);
 	}
 
 	for (int i = 1; i <= n; i++)
 	{
-		Sumdata *= n;
+		Sumdata *= (double)i;
+
+		printf("%d 번째 값: %lf\n",i,Sumdata);
 	}
 	return 0;
 }
