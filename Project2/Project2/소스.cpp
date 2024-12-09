3ce4ac4c243215f3564208cc8e2f3c97e09d75ed
killpop714 <112853297+killpop714@users.noreply.github.com>
2024-12-09 12:28:30 +0900

    dd

--- Project2/Project2/소스.cpp
+++ Project2/Project2/소스.cpp
@@ -76,34 +76,123 @@
 //}
 
 
-int fac() {
-	int result = 1;
-	int n = 0;
-	printf("반복횟수:");
-	scanf_s("%d", &n);
-	for (int i = 0; i <= n; i++)
-	{
-		result += i;
-	}
-	return result;
-}
+//int fac() {
+//	int result = 1;
+//	int n = 0;
+//	printf("반복횟수:");
+//	scanf_s("%d", &n);
+//	for (int i = 0; i <= n; i++)
+//	{
+//		result += i;
+//	}
+//	return result;
+//}
+//
+//int get_tax(int income)
+//{
+//	int result = 0;
+//
+//	if (income > 1000)
+//		return result = 1000 * 0.08 + (income - 1000) * 0.1;
+//	else
+//		return result = income * 0.08;
+//
+//}
 
-int get_tax(int income)
-{
-	int result = 0;
 
-	if (income > 1000)
-		return result = 1000 * 0.08 + (income - 1000) * 0.1;
-	else
-		return result = income * 0.08;
+//void factory() {
+//	//	long long n, result = 1, i;
+//	//	printf("정수를 입력하세요.");
+//	//	scanf_s("%lld", &n);
+//	//	for (i = 1; i <= n; i++) {
+//	//		result = result + i;
+//	//		printf("결과 = %lld\n\n", result);
+//	//	}
+//	//}
+//	//
+//	//void sn() {
+//	//	double a, result;
+//	//	printf("각도를 적으시오.");
+//	//	scanf_s("%lf", &a);
+//	//	result = sin(a);
+//	//	printf("결과 = %lf\n\n", result);
+//	//}
+//	//
+//	//void loglog() {
+//	//	double a, result;
+//	//	printf("실수값을 입력하세요: ");
+//	//	scanf_s("%lf", &a);
+//	//	if (a <= 0.0)
+//	//		printf("오류\n");
+//	//	else {
+//	//		result = log10(a);
+//	//		printf("결과 : %lf\n\n", result);
+//	//	}
+//	//}
+#include <math.h>  
+#define STUDENTS 10
 
-}
 
+//넘모 쉽다
 int main()
 {
-	int income = 0;
-	printf("소득을 입력하세요(만원):");
-	scanf_s("%d", &income);
-	get_tax(income);
-	printf("소득세는:%d(만원)", get_tax(income));
+	int scores[STUDENTS];
+	int sum = 0;
+	int i, arg;
+	for (i = 0; i < STUDENTS; i++) {
+		printf("학생을 즐겁게 할 점수를 지정하세요:");
+		scanf_s("%d", &scores[i]);
+		sum += scores[i];
+	}
+
+	arg = sum / STUDENTS;
+	printf("성적 평균: %d", arg);
+
+	return 0;
 }
+//void addM(int *m) {
+//	int n;
+//
+//	printf("얼마를 입금하시겠습니까:");
+//	scanf_s("%d", &n);
+//	*m = *m + n;
+//}
+//
+//void subM(int* m) {
+//	int n;
+//	printf("얼마를 출금하시겠습니까:");
+//	scanf_s("%d", &n);
+//	if (*m - n > 0)
+//	{
+//		*m = *m - n;
+//		printf("%d 출금 하였습니다\n", n);
+//	}
+//	else
+//		printf("잔액이 부족하여 출금할 수 없습니다\n");
+//	
+//}
+//
+//
+//int main()
+//{
+//	int money = 0;
+//	while (1) {
+//		switch (menu())
+//		{
+//		case 1:
+//			printf("현재 잔액: %d\n", money);
+//			break;
+//		case 2:
+//			addM(&money);
+//			break;
+//		case 3:
+//			subM(&money);
+//			break;
+//		case 4:
+//			return 0;
+//		default:
+//			printf("잘못 출력하였습니다\n.");
+//			break;
+//		}
+//	}
+//}
